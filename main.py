f85bbede7decca69c62283159d185af6f4f5fee7
TARA6567 <156179081+TARA6567@users.noreply.github.com>
2024-03-20 10:04:24 +0000

    add dog spike

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print(1+1)
+class  DogSpike:
+    name= 'Spike'
+    def __int__(self):
+        self_is_walked = False
+    def walk (self):
+        print('Dog walked')
+        self.is_walked = True
+
+my_dog  = DogSpike
+print(my_dog.name) #'Spike'
+# print(my_dog is_walked)#False
+# my_dog. walk ()
+# print (my_dog is_walked)#True
